a6fd3c235dd202c0a3470279b610684bcded0c59
Biraj Ghorasaini <biraj.growcts@gmail.com>
2025-08-11 10:31:44 +0545

    started day2 practice on vuejs and complete understanding about v-binding

---
+++ practices/day2/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AttributeBindingDemo from './components/AttributeBindingDemo.vue';
+</script>
+<template>
+  <div class="app">
+    <AttributeBindingDemo />
+  </div>
+</template>
+
+<style scoped>
+.app {
+  text-align: center;
+  margin-top: 50px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
