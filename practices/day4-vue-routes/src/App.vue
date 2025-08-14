646357c520368c2d576e1f7d2f3b802df49a5f0d
Biraj Ghorasaini <biraj.growcts@gmail.com>
2025-08-14 07:24:15 +0545

    started learning vue routing

---
+++ practices/day4-vue-routes/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view></router-view>
+</template>
+
+<style scoped></style>
---
+++ practices/day4-vue-routes/src/views/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+<template>
+<div class="home">
+    <h2>Hii, I am Home page.</h2>
+</div>    
+</template>
+
+<style scoped>
+
+</style>
